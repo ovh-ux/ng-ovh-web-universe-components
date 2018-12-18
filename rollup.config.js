a779d10b4eaa7c53d49d23084f8b0403f7248bda
Florian Renaut <florian.renaut@corp.ovh.com>
2018-12-18 11:27:33 +0100

    fix(deps): add missing peer dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -7,16 +7,19 @@ const config = configGenerator({
 export default [
   config.cjs(),
   config.umd({
-    globals: {
-      '@uirouter/angularjs': 'uiRouter',
-      angular: 'angular',
-      'angular-translate': 'pascalprecht.translate',
-      'ipaddr.js': 'ipaddr',
-      jquery: '$',
-      lodash: '_',
-      moment: 'moment',
-      punycode: 'punycode',
-      URIjs: 'URI',
+    output: {
+      globals: {
+        '@ovh-ux/translate-async-loader': 'translate-async-loader',
+        '@uirouter/angularjs': 'uiRouter',
+        angular: 'angular',
+        'angular-translate': 'pascalprecht.translate',
+        'ipaddr.js': 'ipaddr',
+        jquery: '$',
+        lodash: '_',
+        moment: 'moment',
+        punycode: 'punycode',
+        URIjs: 'URI',
+      },
     },
   }),
 ];
--- src/tabs/index.js
+++ src/tabs/index.js
@@ -2,13 +2,9 @@ import angular from 'angular';
 import translate from 'angular-translate';
 import translateAsyncLoader from '@ovh-ux/translate-async-loader';
 
+import '@ovh-ux/ovh-utils-angular';
 import 'angular-ui-bootstrap';
-
-/* eslint-disable import/no-webpack-loader-syntax, import/no-unresolved, import/extensions */
-import 'script-loader!@ovh-ux/ovh-utils-angular/bin/ovh-utils-angular.min.js';
-import 'script-loader!@ovh-ux/ovh-utils-angular/lib/core.js';
-import 'script-loader!df-tab-menu/build/df-tab-menu.min.js';
-/* eslint-enable import/no-webpack-loader-syntax, import/no-unresolved, import/extensions */
+import 'df-tab-menu';
 
 import wucOvhTabsDirective from './tabs.directive';
 
