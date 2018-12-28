d760361a36b47792eecd43c77ced6cd054c5f0e6
Marie JONES <marie.jones@corp.ovh.com>
2018-12-28 16:21:19 +0100

    feat(autorenew-invite): add autorenew-invite component

---
+++ src/autorenew-invite/index.js
@@ -0,0 +1,27 @@
+import angular from 'angular';
+import '@ovh-ux/ng-payment-method';
+import 'ovh-angular-user-pref';
+import 'ovh-api-services';
+import 'ovh-ui-angular';
+import 'angular-translate';
+import '@ovh-ux/translate-async-loader';
+
+import { SUBSIDIARIES_WITH_OPTIONAL_AUTORENEW } from './autorenew-invite.constant';
+import WucAutorenewInvite from './autorenew-invite.component';
+
+const moduleName = 'wucAutorenewInvite';
+
+angular
+  .module(moduleName, [
+    'ngOvhPaymentMethod',
+    'ovh-angular-user-pref',
+    'ovh-api-services',
+    'oui',
+    'pascalprecht.translate',
+    'translate-async-loader',
+  ])
+  .constant('WUC_SUBSIDIARIES_WITH_OPTIONAL_AUTORENEW', SUBSIDIARIES_WITH_OPTIONAL_AUTORENEW)
+  .component('wucAutorenewInvite', WucAutorenewInvite)
+  .run(/* @ngTranslationsInject ./translations */);
+
+export default moduleName;
